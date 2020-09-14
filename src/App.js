d5f7285c15491afe38c26e044b61df56fdf136d6
jay <dyjay15@gmail.com>
2020-09-14 14:23:00 +0800

    make logs and searchbar components and bring it to the app js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import SearchBar from './componets/layouts/SearchBar';
+import Logs from './componets/logs/Logs';
 
 function App() {
   useEffect(() => {
     //Init materialize JS
     M.AutoInit();
   });
-  return <div className='App'>Hello World</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
