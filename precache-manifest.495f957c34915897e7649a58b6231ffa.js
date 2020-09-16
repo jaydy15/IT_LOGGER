ec8bb867de822bad009608bfe53df03a72e747d6
jay <dyjay15@gmail.com>
2020-09-16 15:05:13 +0800

    Updates

---
+++ precache-manifest.495f957c34915897e7649a58b6231ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847055d41d067671b58ac3cfd09af2dd",
+    "url": "/IT_LOGGER/index.html"
+  },
+  {
+    "revision": "b9bb58fc832fa8c26ec6",
+    "url": "/IT_LOGGER/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "5d737de0b2f2034c6694",
+    "url": "/IT_LOGGER/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b9bb58fc832fa8c26ec6",
+    "url": "/IT_LOGGER/static/js/2.14b7817a.chunk.js"
+  },
+  {
+    "revision": "42c838fcc1a0adafd62fbc59a52132cc",
+    "url": "/IT_LOGGER/static/js/2.14b7817a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d737de0b2f2034c6694",
+    "url": "/IT_LOGGER/static/js/main.48f19923.chunk.js"
+  },
+  {
+    "revision": "85904f2c4eefa7f55800",
+    "url": "/IT_LOGGER/static/js/runtime-main.040a313a.js"
+  }
+]);
